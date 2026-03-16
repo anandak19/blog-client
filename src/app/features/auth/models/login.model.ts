fc3ad42983c28e9a266d28fa1dba4060dc940970
Anandakrishnan H <anandak19h@gmail.com>
2026-03-17 00:43:34 +0530

    refactor: add comments on auth model

--- src/app/features/auth/models/login.model.ts
+++ src/app/features/auth/models/login.model.ts
@@ -1,5 +1,5 @@
 import { IUser } from './user.model';
 
 export interface IUserLogin extends Pick<IUser, 'email' | 'password'> {}
-
+// payload in cookie from server
 export interface IPayload extends Pick<IUser, 'email' | 'firstName' | 'lastName' | 'id'> {}
